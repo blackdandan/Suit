ffe777cb43e27f486ca006ee24476fa100f77258
blackdandan <784818984@qq.com>
2021-10-18 22:36:10 +0800

    [Navigation]原来DrawerLayout中，Navigation和NavHostFragment绑定，需要设置DrawerLayout为根布局，另外DrawerLayout和ActionBar的导航键绑定需要重写onSupportNavigateUp方法

--- app/src/main/java/com/alone/suit/MainActivity.kt
+++ app/src/main/java/com/alone/suit/MainActivity.kt
@@ -9,10 +9,13 @@ import android.provider.MediaStore
 import android.util.Log
 import android.webkit.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.navigation.NavigationView
 import java.io.File
@@ -27,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
-
+    var appBarConfiguration:AppBarConfiguration? = null
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +39,27 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
         val navigationView = findViewById<NavigationView>(R.id.navigation)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).setDrawerLayout(drawerLayout).build()
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.app_name,R.string.app_name)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
         NavigationUI.setupWithNavController(navigationView, navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration!!)
+    }
+
+    override fun onBackPressed() {
+        Log.d(TAG, "onBackPressed: ")
+        super.onBackPressed()
+    }
+
+    override fun navigateUpTo(upIntent: Intent?): Boolean {
+        Log.d(TAG, "navigateUpTo: ")
+        return super.navigateUpTo(upIntent)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d(TAG, "onSupportNavigateUp: ")
+        val navController = findNavController(R.id.nav_host)
+        return navController.navigateUp(appBarConfiguration!!) || super.onSupportNavigateUp()
     }
 }
